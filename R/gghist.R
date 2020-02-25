7906fb1d7a04c96f1f500718afcb34f87917d88c
Lesley <iamthelm@gmail.com>
2020-02-24 20:53:27 -0800

    added the docstring for histogram function

---
+++ R/gghist.R
@@ -0,0 +1,16 @@
+#' Make a ggplot histogram
+#'
+#' Creates a ggplot histogram indicating the position of the mean and median and displays the standard deviation.
+#'
+#' @param data A tibble or dataframe.
+#' @param variable The column name inside data that will be made into a histogram.
+#'
+#' @return Produces a ggplot histogram with mean, median and standard deviation indicated on the plot.
+#' @export
+#'
+#' @examples
+#' gghist(iris, Sepal.Length)
+gghist <- function(data, variable) {
+  # guts of function goes here
+
+}
