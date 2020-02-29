f62c2f9c2217770814caf7d9706b65c67f986e09
thejacktan <59751623+thejacktan@users.noreply.github.com>
2020-02-29 11:42:36 -0800

    Added docstring for Fourier transform function

---
+++ R/fourier_transform.R
@@ -0,0 +1,19 @@
+#' Make a Fourier transform plot of the data specified in the 'time_col' column using ggplot2
+#'
+#' @param data dataframe containing all necessary information
+#' @param time_col string containing the time data column corresponding to the data we
+#'                 want to apply Fourier transform to
+#' @param data_col string containing the name of the column we want to apply Fourier transform to
+#'
+#' @return Produces a ggplot2 line plot of frequency vs amplitude of the data inputed
+#' @export
+#'
+#' @examples
+#' fourier_transform(data = my_data,
+#'                   time_col = 'time_series',
+#'                   data_col = 'signal')
+
+
+fourier_transform <- function(data, time_col, data_col) {
+# guts of function goes here
+}
