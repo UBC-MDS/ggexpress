d69d85ff6056bea43a3686febe34bceaa8117828
puddingz <757767850@qq.com>
2020-03-04 18:17:35 -0800

    first draft of ts_plot

--- R/ts_plot.R
+++ R/ts_plot.R
@@ -1,19 +1,31 @@
+library(ggplot2)
+
 #' Time series decomposition and plot
 #'
 #' Convert csv file into a time series object, decompose it into in trend, seasonality/cyclicity
 #' and noise/remainder/error and plot the raw data and the decomposed components.
 #'
 #' @param data str: the path of the csv file
-#' @param type str: "regular" or "irregular"
-#'            "regular": time series with regular time intervals
-#'            "irregular": time series with irregular time intervals
 #' @param col str: the column to be analyzed
 #' @param frequency int: the desired time interval
 #'
-#' @return a figure with 4 subplots
+#' @return ggplot Object
 #' @export
 #' @examples
 #' ts_plot("https://raw.githubusercontent.com/plotly/datasets/master/timeseries.csv", A, irregular, 3)
-ts_plot <- function(data, col, type, frequency){
-  print("hello")
+ts_plot <- function(data, col, frequency){
+  df <- read_csv(data)
+  ts <- ts(df[[col]], frequency = frequency)
+
+  if (frequency == 1){
+    plt <- ggplot2::autoplot(ts, main = "Time series data", xlab="Time", ylab = col)
+  }
+  else {
+    plt <- ts %>%
+      decompose() %>%
+      ggplot2::autoplot(main = "Decomposed Time series data", xlab="Time", ylab = col)
+  }
+  return(plt)
 }
+
+ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_lab1_puddingz/master/data/ts4_jjshares.csv?token=AAAAOM22MPFSPEHAUMEANOS6NGM5A", "earnings", 4)
