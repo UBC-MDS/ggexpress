e1251c428b9c4ed0bc006fd541380d9220a002ac
puddingz <757767850@qq.com>
2020-03-04 20:51:35 -0800

    add first part of unit test

--- tests/testthat/test-ts_plot.R
+++ tests/testthat/test-ts_plot.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+require(testthat)
+require(ggplot2)
+require(forecast)
+
+test_that("The plot has correct number of layers", {
+  test_plt1 <- ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts4_jjshares.csv?token=AAAAOMZQBS5IDU4Q6K3RBXS6NG7NM",
+                      "earnings", 4)
+  expect_equal(length(test_plt1$layers), 4)
+
+  test_plt2 <- ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts1_globaltemp.csv?token=AAAAOM4GFOSICJVSVP2YUP26NG7QE",
+                       "temp", 1)
+  expect_equal(length(test_plt2$layers), 1)
+
 })
