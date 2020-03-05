c39ea5bae39e4eccd4fad497cd0ee91d82a8f58a
puddingz <757767850@qq.com>
2020-03-04 18:29:18 -0800

    add package

--- R/ts_plot.R
+++ R/ts_plot.R
@@ -1,5 +1,5 @@
 library(ggplot2)
-
+library(dplyr)
 #' Time series decomposition and plot
 #'
 #' Convert csv file into a time series object, decompose it into in trend, seasonality/cyclicity
@@ -12,9 +12,9 @@ library(ggplot2)
 #' @return ggplot Object
 #' @export
 #' @examples
-#' ts_plot("https://raw.githubusercontent.com/plotly/datasets/master/timeseries.csv", A, irregular, 3)
+#' ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_lab1_puddingz/master/data/ts4_jjshares.csv?token=AAAAOM22MPFSPEHAUMEANOS6NGM5A", "earnings", 4)
 ts_plot <- function(data, col, frequency){
-  df <- read_csv(data)
+  df <- readr::read_csv(data)
   ts <- ts(df[[col]], frequency = frequency)
 
   if (frequency == 1){
