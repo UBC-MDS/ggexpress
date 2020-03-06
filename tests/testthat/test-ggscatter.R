13f1a0226c9a88bd7d766c1dc1b613eaeafa59c0
tphaterp <tphaterp@gmail.com>
2020-03-06 15:22:56 -0800

    added log_transformation tests

--- tests/testthat/test-ggscatter.R
+++ tests/testthat/test-ggscatter.R
@@ -10,6 +10,8 @@ test_that('Plot should use geom_point and correctly map variables to the correct
 
 standard_plot <- scatter_express(mtcars, mpg, qsec)
 iris_plot <- scatter_express(iris, Sepal.Width, Sepal.Length)
+log_transformed_mtcars <- scatter_express(mtcars, mpg, qsec, x_transform = TRUE, y_transform = TRUE)
 
 vdiffr::expect_doppelganger("mtcars scatterplot", standard_plot)
 vdiffr::expect_doppelganger("iris_scatterplot", iris_plot)
+vdiffr::expect_doppelganger("logmtcars_scatter", log_transformed_mtcars)
