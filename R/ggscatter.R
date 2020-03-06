bf95172cd66f70a68fc9b7a47cf0eec7a8577b22
tphaterp <tphaterp@gmail.com>
2020-03-06 12:50:08 -0800

    added log transformations to my function and added unit tests

--- R/ggscatter.R
+++ R/ggscatter.R
@@ -9,25 +9,38 @@ library(ggplot2)
 #' @param df Dataframe to plot
 #' @param xval x-var Column name used as the x-axis variable
 #' @param yval y-var Column name used as the y-axis variable
+#' @param x_transform Determines whether the x-axis undergoes a natural log transformation
+#' @param y_transform Determines whether the y-axis undergoes a natural log transformation
 #'
 #' @return ggplot Object
 #' @export
 #'
 #' @examples
-#' scatter_express(hiris, "Sepal.Width, Petal.Length)
-scatter_express <- function(df, xval = NA, yval = NA){
+#' scatter_express(iris, "Sepal.Width, Petal.Length)
+scatter_express <- function(df, xval = NA, yval = NA, x_transform = FALSE, y_transform = FALSE){
 
   corr_df <- dplyr::select(df, {{xval}}, {{yval}})
+
+  if (is.numeric(df[[1]]) == FALSE) stop("Your x-variable must be numeric")
+  if (is.numeric(df[[2]]) == FALSE) stop("Your y-variable must be numeric")
+
   corr_val <- round(stats::cor(df[[1]], df[[2]]), 2)
 
 
-  scatter <- ggplot2::ggplot(df, ggplot2::aes(x = {{xval}}, y = {{yval}})) + ggplot2::geom_point()
+  scatter <- ggplot2::ggplot(df, ggplot2::aes(x = {{xval}}, y = {{yval}})) +
+    ggplot2::geom_point(color = "blue")
 
-  scatter <- scatter + ggplot2::labs(title = paste(rlang::get_expr(scatter$mapping$x), " vs ", rlang::get_expr(scatter$mapping$y), "(Correlation: ",  corr_val, ")"))
+  if (x_transform == TRUE){
+    scatter <- scatter + scale_x_continuous(trans= "log2")
+
+  }
+
+  if (y_transform == TRUE){
+    scatter <- scatter + scale_y_continuous(trans = "log2")
+  }
+
+  scatter <- scatter + ggplot2::labs(title = paste(rlang::get_expr(scatter$mapping$x), " vs ", rlang::get_expr(scatter$mapping$y), "(Pearson Correlation: ",  corr_val, ")"))
   scatter
 }
 
-
-df <- iris
-
-plot <- scatter_express(df, Sepal.Width, Petal.Length)
+test <- scatter_express(iris, Sepal.Length, Sepal.Length, x_transform = FALSE, y_transform = TRUE)
---
+++ tests/testthat/test-ggscatter.R
@@ -0,0 +1,15 @@
+test_that('Plot should use geom_point and correctly map variables to the correct axis.', {
+  plot <- scatter_express(mtcars, drat, wt)
+  expect_true("GeomPoint" %in% c(class(plot$layers[[1]]$geom)))
+  expect_true("drat"  == rlang::get_expr(plot$mapping$x))
+  expect_true("wt" == rlang::get_expr(plot$mapping$y))
+
+  expect_true("drat" == plot$labels$x)
+  expect_true("wt" == plot$labels$y)
+})
+
+standard_plot <- scatter_express(mtcars, mpg, qsec)
+iris_plot <- scatter_express(iris, Sepal.Width, Sepal.Length)
+
+vdiffr::expect_doppelganger("mtcars scatterplot", standard_plot)
+vdiffr::expect_doppelganger("iris_scatterplot", iris_plot)
