b5e3cd97bda1f4ee1fd860bdfc877fcecaccef27
puddingz <757767850@qq.com>
2020-03-07 10:29:55 -0800

    edit the tests

--- R/ts_plot.R
+++ R/ts_plot.R
@@ -2,55 +2,56 @@
 #'
 #' Convert csv file into a time series object, decompose it into trend, seasonality/cyclicity
 #' and noise/remainder/error and plot the raw data and the decomposed components.
+#' If the time series can't be decomposed, the function will just return the line chart of the raw data. 
 #'
 #' @param data str: the path of the csv file
 #' @param col str: the column to be analyzed
-#' @param frequency int: the desired time interval
+#' @param frequency int: should choose from {1, 4, 12, 52}
 #'
 #' @return ggplot Object
 #' @export
 #' @examples
 #' ts_plot("~/Desktop/ts4_jjshares.csv", "earnings", 4)
 ts_plot <- function(data, col, frequency){
-
+  
   # basic check of input
-  if(length(readr::read_csv(data)) == 1){
-    stop("The type of the input data must be a csv file")
-  }
-
   if(typeof(data) != 'character'){
     stop("The path of the csv file should be a string")
   }
-
+  
+  if(length(readr::read_csv(data)) == 1){
+    stop("The type of the input data must be a csv file")
+  }
+  
+  
   if(typeof(col) != 'character') {
     stop("The column name should be a string")
   }
-
+  
   if(typeof(frequency) != "double") {
-    stop("The frequency of time series should be an integer")
+    stop("The frequency of time series should be an integer chosen from 1, 4, 12, 52")
   }
-
+  
   if(frequency %in% c(1, 4, 12, 52) == FALSE) {
-    stop("The frequency of time series should be annual/quarterly/monthly/weekly")
+    stop("The frequency of time series should be an integer chosen from 1, 4, 12, 52")
   }
-
-
+  
   # read the data into a dataframe
   df <- readr::read_csv(data)
-
+  
   # check the input further
   if (c(col) %in% colnames(df) == FALSE){
     stop("The column names were not found")
   }
-
+  
   # convert csv file into a time series object
   ts <- ts(df[[col]], frequency = frequency)
-
+  
   # check the time series object
   if (time(ts)[2] - time(ts)[1] != time(ts)[3] - time(ts)[2]){
     stop("The interval of the time series should be the same.")
   }
-
+  
   # plot the raw data and the decomposed components
   if (frequency == 1){
     plt <- forecast::autoplot(ts, main = "Time series data", xlab="Time", ylab = col)
@@ -62,4 +63,3 @@ ts_plot <- function(data, col, frequency){
   return(plt)
 }
 
-ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_lab1_puddingz/master/data/ts4_jjshares.csv?token=AAAAOM22MPFSPEHAUMEANOS6NGM5A", "earnings", 4)
--- tests/testthat/test-ts_plot.R
+++ tests/testthat/test-ts_plot.R
@@ -12,6 +12,7 @@ test_ts_plot <- function() {
   # import helper data
   data1 <- "https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts4_jjshares.csv?token=AAAAOMZQBS5IDU4Q6K3RBXS6NG7NM"
   data2 <- "https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts1_globaltemp.csv?token=AAAAOM4GFOSICJVSVP2YUP26NG7QE"
+  data3 <- "https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts6_shorelines.csv?token=AAAAOM2VTEDBLBFBZPV7UW26NHKOU"
   
   test_data_1 <- ts_plot(data1, "earnings", 4)
   test_data_2 <- ts_plot(data2, "temp", 1)
@@ -20,12 +21,12 @@ test_ts_plot <- function() {
   test_that("The returned plot should be a ggplot object.", {
     expect_true(ggplot2::is.ggplot(test_data_1))
   })
-
+  
   test_that("The plot for annual time series should contain only 1 line chart", {
     expect_equal(length(test_data_2$layers), 1)
     expect_true("GeomLine" %in% c(class(test_data_2$layers[[1]]$geom)))
   })
-
+  
   test_that("The plot for quarterly time series should contain 4 subplots", {
     expect_equal(length(test_data_1$layers), 4)
     expect_true("GeomLine" %in% c(class(test_data_1$layers[[1]]$geom)))
@@ -33,48 +34,42 @@ test_ts_plot <- function() {
     expect_true("GeomHline" %in% c(class(test_data_1$layers[[3]]$geom)))
     expect_true("GeomRect" %in% c(class(test_data_1$layers[[4]]$geom)))
   })
-
+  
   test_that("Corresponding error message should be expected if the data path argument doesn't link to a csv file.", {
     expect_error(ts_plot("https://github.ubc.ca/MDS-2019-20/DSCI_574_lab1_puddingz/blob/master/data/ts4_jjshares.csv",
                          "earnings", 4),
-    regexp = "The type of the input data must be a csv file")
+                 regexp = "The type of the input data must be a csv file")
   })
   
   test_that("Corresponding error message should be expected if the data path argument is not a string.", {
-    expect_error(ts_plot(123,
-                         "earnings", 4),
+    expect_error(ts_plot(123, "earnings", 4),
                  regexp = "The path of the csv file should be a string")
   })
-
+  
   test_that("Corresponding error message should be expected if the col argument is not a string", {
-    expect_error(ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts4_jjshares.csv?token=AAAAOMZQBS5IDU4Q6K3RBXS6NG7NM",
-                         2, 4),
+    expect_error(ts_plot(data1, 2, 4),
                  regexp = "The column name should be a string")
   })
   
   test_that("Corresponding error message should be expected if the frequency argument is not an integer", {
-    expect_error(ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts4_jjshares.csv?token=AAAAOMZQBS5IDU4Q6K3RBXS6NG7NM",
-                         "earnings", "4"),
-                 regexp = "The frequency of time series should be an integer")
+    expect_error(ts_plot(data1, "earnings", "4"),
+                 regexp = "The frequency of time series should be an integer chosen from 1, 4, 12, 52")
   })
-
+  
   test_that("Corresponding error message should be expected if the frequency argument is not proper", {
-    expect_error(ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts4_jjshares.csv?token=AAAAOMZQBS5IDU4Q6K3RBXS6NG7NM",
-                         "earnings", 5),
-                 regexp = "The frequency of time series should be annual/quarterly/monthly/weekly")
+    expect_error(ts_plot(data1, "earnings", 5),
+                 regexp = "The frequency of time series should be an integer chosen from 1, 4, 12, 52")
   })
-
+  
   test_that("Corresponding error message should be expected if the col argument is not a column name", {
-    expect_error(ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts4_jjshares.csv?token=AAAAOMZQBS5IDU4Q6K3RBXS6NG7NM",
-                         "earning", 4),
+    expect_error(ts_plot(data1, "earning", 4),
                  regexp = "The column names were not found")
   })
-
-
+  
+  
   test_that("Corresponding error message should be expected if the time series is irregular.", {
-    expect_error(ts_plot("https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-mod_students/master/labs/release/lab1/data/ts6_shorelines.csv?token=AAAAOM2VTEDBLBFBZPV7UW26NHKOU",
-                         "shoreline", 12),
+    expect_error(ts_plot(data3, "shoreline", 12),
                  regexp = "The interval of the time series should be the same.")
   })
-
+  
 }
