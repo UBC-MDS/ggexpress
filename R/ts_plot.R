639cbbb04f8f5b71db9d362e72bd71e22a9456cb
puddingz <757767850@qq.com>
2020-03-13 15:12:12 -0700

    Edit function, tests and vignettes

--- R/ts_plot.R
+++ R/ts_plot.R
@@ -1,8 +1,8 @@
 #' Time series decomposition and plot
 #'
-#' Convert csv file into a time series object, decompose it into trend, seasonality/cyclicity
+#' Convert a dataframe into a time series object, decompose it into trend, seasonality/cyclicity
 #' and noise/remainder/error and plot the raw data and the decomposed components.
-#' If the time series can't be decomposed, the function will just return the line chart of the raw data. 
+#' If the time series can't be decomposed, the function will just return the line chart of the raw data.
 #'
 #' @param data str: the path of the csv file
 #' @param col str: the column to be analyzed
@@ -11,47 +11,39 @@
 #' @return ggplot Object
 #' @export
 #' @examples
-#' ts_plot("~/Desktop/ts4_jjshares.csv", "earnings", 4)
+#' time <- c("1950 Q1", "1950 Q2", "1950 Q3", "1950 Q4",
+#'           "1951 Q1", "1951 Q2", "1951 Q3", "1951 Q4")
+#' earnings <- c(0.71, 0.63, 0.82, 0.91, 0.71, 0.63, 0.82, 0.91)
+#' ts_data <- tibble::tibble(time, earnings)
+#' ts_plot(data=ts_data, col="earnings", frequency=4)
 ts_plot <- function(data, col, frequency){
-  
+
   # basic check of input
-  if(typeof(data) != 'character'){
-    stop("The path of the csv file should be a string")
+  if (!class(data)[1] %in% c("tbl_df", "tbl", "data.frame")) {
+    stop("Data must be of type tibble or data.frame")
   }
-  
-  if(length(readr::read_csv(data)) == 1){
-    stop("The type of the input data must be a csv file")
-  }
-  
-  
+
+
   if(typeof(col) != 'character') {
     stop("The column name should be a string")
   }
-  
+
   if(typeof(frequency) != "double") {
     stop("The frequency of time series should be an integer chosen from 1, 4, 12, 52")
   }
-  
+
   if(frequency %in% c(1, 4, 12, 52) == FALSE) {
     stop("The frequency of time series should be an integer chosen from 1, 4, 12, 52")
   }
-  
-  # read the data into a dataframe
-  df <- readr::read_csv(data)
-  
+
   # check the input further
-  if (c(col) %in% colnames(df) == FALSE){
+  if (c(col) %in% colnames(data) == FALSE){
     stop("The column names were not found")
   }
-  
+
   # convert csv file into a time series object
-  ts <- ts(df[[col]], frequency = frequency)
-  
-  # check the time series object
-  if (time(ts)[2] - time(ts)[1] != time(ts)[3] - time(ts)[2]){
-    stop("The interval of the time series should be the same.")
-  }
-  
+  ts <- stats::ts(data[[col]], frequency = frequency)
+
   # plot the raw data and the decomposed components
   if (frequency == 1){
     plt <- forecast::autoplot(ts, main = "Time series data", xlab="Time", ylab = col)
